d836282ad24bab2c7b86493a540401f73143bf0c
judlejudle <judean2021@gmail.com>
2023-04-15 00:11:06 +0900

    Learned router, controller, parameter

---
+++ src/controllers/videoController.js
@@ -0,0 +1,6 @@
+export const trending = (req,res) => res.send("Trending Videos")
+export const see = (req,res) => res.send("See Videos")
+export const edit = (req,res) => res.send("Edit Videos")
+export const search = (req,res) => res.send("Search Videos")
+export const upload = (req,res) => res.send("Upload Videos")
+export const deleteVideo = (req,res) => res.send("Delete Videos")
